9b2c4fe1b32c1350bd3252252d1521eda516d7d7
five <tknl45@hotmail.com>
2021-09-04 16:09:59 +0800

    init

---
+++ Controllers/TestController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WriteCookieAndRedirect.Controllers
+{
+    [ApiController]
+    [Route("test")]
+    public class TestController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<TestController> _logger;
+
+        public TestController(ILogger<TestController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("forecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet]
+        [Route("WriteCookieAndRedir")]
+        public RedirectResult  WriteCookieAndRedir(string key, string value, string url)
+        {
+            //寫 cookie
+            Response.Cookies.Append(key, value);
+            Response.Cookies.Append(key, value, new CookieOptions() { Domain = ".cht.com.tw", HttpOnly = true});
+
+            //重導
+            return RedirectPermanent(url);
+        }
+    }
+}
